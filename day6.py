919a79f32689640f53ea5968115b566a08ca3636
Darrel Chang <darrelchang@gmail.com>
2025-12-07 19:11:38 -0800

    day6 part2

--- day6.py
+++ day6.py
@@ -1,6 +1,14 @@
-def parseLines(fileName):
+def parseLinesPart1(fileName):
     with open(fileName, 'r') as f:
         return [line.split() for line in f.readlines()]
+
+def parseLinesPart2(fileName):
+    with open(fileName, 'r') as f:
+        input_data = f.read()
+        data = input_data.splitlines()
+        nums = [ ''.join(num) for num in zip(*data[:-1])]
+        ops = data[-1].split()
+        return nums, ops
 
 def part1(lines):
     res = 0
@@ -21,6 +29,39 @@ def part1(lines):
                         acc *= int(lines[i][j])
         res += acc
     return res
-lines = parseLines("day6_input.txt")
-res = part1(lines)
-print(res)
+
+def part2(nums, ops):
+    i = 0
+    op = ops[i]
+    res = 0
+    acc = 0
+    for num in nums:
+        try:
+            num = int(num)
+            if op == "*":
+                if acc:
+                    acc *= num
+                else:
+                    acc = num
+            if op == "+":
+                if acc:
+                    acc += num
+                else:
+                    acc = num
+        except:
+            i += 1
+            op = ops[i]
+            res += acc
+            acc = 0
+    res += acc
+    return res
+
+if __name__ == "__main__":
+    lines = parseLinesPart1("day6_test_input.txt")
+    print(lines)
+    part1Res = part1(lines)
+    print(part1Res)
+
+    nums, ops = parseLinesPart2("day6_input.txt")
+    part2Res = part2(nums, ops)
+    print(part2Res)
